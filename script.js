14c369ab4fb1ac410e593ab74957a50e75ac5df3
Antti Heikkinen <6470546+CarelessAntics@users.noreply.github.com>
2026-05-10 01:07:56 +0300

    Restructuring the code file to be a bit neater

--- script.js
+++ script.js
@@ -2,6 +2,90 @@ const output = document.querySelector('.calc-out');
 const calcBody = document.querySelector('.calc-body');
 const calcKeys = document.querySelector('.calc-keys');
 
+// TODO output section for the calculator
+// TODO make pretty
+
+// ====================================
+// CALCULATOR VISUALS / USER FACING
+// ====================================
+
+const numKeys = createNumKeys();
+const equalsKey = makeKey("=");
+
+calcKeys.appendChild(numKeys);
+calcKeys.appendChild(equalsKey);
+
+calcBody.addEventListener('click', keyClick);
+addEventListener('keydown', keyPress);
+
+function keyClick(event) {
+    let target = event.target
+    if (target.tagName === 'SPAN'){
+        target = target.parentNode;
+    }
+    if (target.id.includes('key')) {
+        const buttonValue = target.firstChild.textContent
+        buildFormula(buttonValue);
+    }
+}
+
+
+function keyPress(event) {
+    buildFormula(event.key);
+}
+
+
+function createNumKeys() {
+    // Current layout generator switches rows after 4 elements
+     const numbers = ['(', ')', '<=', 'CLEAR',
+                      7, 8, 9, '/',
+                      4, 5, 6, '*',
+                      1, 2, 3, '-',
+                      ' ', 0, '.', '+'];
+
+    const elementsPerRow = 4
+    let counter = 0;
+    let row;
+    const numberKeys = document.createElement('div')
+
+    for (num of numbers) {
+        if (counter % elementsPerRow === 0) {
+            if (row) {
+                numberKeys.appendChild(row);
+            }
+            row = document.createElement('div');
+            row.classList.add('key-row');
+        }
+
+        let numKey = makeSquare(makeKey(num));
+        row.appendChild(numKey);
+        counter++;
+    }
+    numberKeys.appendChild(row);
+    //calcKeys.appendChild(numberKeys);
+
+    return numberKeys
+}
+
+function makeKey(key) {
+    let elem = document.createElement('div');
+    elem.id = `key${key}`;
+    elem.classList.add('key');
+    elem.innerHTML = `<span>${key}</span>`;
+
+    return elem;
+}
+
+function makeSquare(elem) {
+    elem.classList.add("aspect-1");
+    return elem;
+}
+
+
+// ====================================
+// EXPRESSION OBJECT
+// ====================================
+
 // Object for storing and building the calculation expression
 // Numbers are added to a buffer, and once an operator is selected or a parenthesis added, the buffer is pushed into an array
 // Numerical indices contain an array of the current expression at a certain depth
@@ -71,41 +155,9 @@ function Expression() {
 
 var inputExpression = new Expression();
 
-// ==================================================
-// Build the calculator in JS:
-
-// Numbers and operators in the middle
-const numKeys = createNumKeys();
-
-// Equals key at the bottom
-const equalsKey = makeKey("=");
-
-calcKeys.appendChild(numKeys);
-calcKeys.appendChild(equalsKey);
-
-
-// Buttonclicks
-calcBody.addEventListener('click', keyClick);
-addEventListener('keydown', keyPress);
-
-
-function keyClick(event) {
-    // TODO split into functions that can also work with keypresses
-    let target = event.target
-    if (target.tagName === 'SPAN'){
-        target = target.parentNode;
-    }
-
-    if (target.id.includes('key')) {
-        const buttonValue = target.firstChild.textContent
-        buildFormula(buttonValue);
-    }
-}
-
-function keyPress(event) {
-    buildFormula(event.key);
-}
-
+// ====================================
+// CALCULATOR LOGIC
+// ====================================
 
 function buildFormula(buttonValue) {
     const operators = ['+', '/', '*', '-']
@@ -114,13 +166,14 @@ function buildFormula(buttonValue) {
     const isOperator = operators.includes(buttonValue) 
                        && (inputExpression.num || Array.isArray(inputExpression.getCurrent().at(-1))); // Either numbers exist in the buffer or we just closed parentheses
     const isOperatorChange = operators.includes(buttonValue) // is operator
-                            && !inputExpression.num // Current number string is empty
+                            && !inputExpression.num // Current number buffer is empty
                             && typeof inputExpression.getCurrent().at(-1) === 'string' // Previous element is string, a.k.a not an array
                             && inputExpression.getCurrent().length; // Current array isn't empty
-    const isDecimal = buttonValue === '.' && !inputExpression.num.includes('.');
+    const isDecimalPoint = (buttonValue === '.' || buttonValue === ',') // I'm a European heathen who allows commas as decimal separators
+                            && !inputExpression.num.includes('.'); // Does the number already have a decimal point in it
     const isDigit = /[0-9]+/.test(buttonValue);
     const isOpenParenthesis = buttonValue === '(';
-    const isCloseParenthesis = buttonValue === ')'; // Last element isn't an operator
+    const isCloseParenthesis = buttonValue === ')';
 
     if (isOperator) {
         inputExpression.addOperator(buttonValue);
@@ -130,7 +183,7 @@ function buildFormula(buttonValue) {
         inputExpression.changeLast(buttonValue);
         console.log(inputExpression.getCurrent());
 
-    } else if (isDecimal) {
+    } else if (isDecimalPoint) {
         inputExpression.addDigit(buttonValue);
         console.log(inputExpression.num);
 
@@ -167,7 +220,6 @@ function buildFormula(buttonValue) {
         let result = parseDepths(inputExpression.getCurrent())
         console.log(result)
         inputExpression = new Expression();
-
     }
 }
 
@@ -245,47 +297,4 @@ function parseSingleDepth(arr) {
     return arr[0];
 }
 
-function createNumKeys() {
-     const numbers = ['(', ')', '<=', 'CLEAR',
-                      7, 8, 9, '/',
-                      4, 5, 6, '*',
-                      1, 2, 3, '-',
-                      ' ', 0, '.', '+']
 
-    let counter = 0;
-    let row;
-
-    const numberKeys = document.createElement('div')
-
-    for (num of numbers) {
-        if (counter % 4 === 0) {
-            if (row) {
-                numberKeys.appendChild(row);
-            }
-            row = document.createElement('div');
-            row.classList.add('key-row');
-        }
-
-        let numKey = makeSquare(makeKey(num));
-        row.appendChild(numKey);
-        counter++;
-    }
-    numberKeys.appendChild(row);
-    //calcKeys.appendChild(numberKeys);
-
-    return numberKeys
-}
-
-function makeKey(key) {
-    let elem = document.createElement('div');
-    elem.id = `key${key}`;
-    elem.classList.add('key');
-    elem.innerHTML = `<span>${key}</span>`;
-
-    return elem;
-}
-
-function makeSquare(elem) {
-    elem.classList.add("aspect-1");
-    return elem;
-}
